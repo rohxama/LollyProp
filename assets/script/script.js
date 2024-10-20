cf255eb0ee8ba7d8255802b1f7ed8345ffae11dd
Rohama Salfi <rohxama@gmail.com>
2024-10-20 13:15:39 -0700

    jquery removal

--- assets/script/script.js
+++ assets/script/script.js
@@ -33,51 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-function onscroll() {
-    let projectSection = document.querySelector(".project-section");
-    let projectWrapper = document.querySelector(".project-wrapper");
-    let projectImage = document.querySelector("#project1_img");
-    let heading = document.querySelector(".desc");
-    let rect = projectImage.getBoundingClientRect();
-    let rect2 = heading.getBoundingClientRect();
-    let projectWrapperRect = projectWrapper.getBoundingClientRect();
 
-
-    if (rect.top <= 290) {
-        projectSection.style.top = '-530px';
-        projectWrapper.style.top = '-219px';
-        projectSection.style.transition = '1s linear';
-        projectWrapper.style.transition = '1s linear';
-
-
-    } else {
-        projectSection.style.top = '0px';
-        projectWrapper.style.top = '0px';
-    }
-
-    if (projectWrapperRect.top <= 290) {
-        projectImage.style.position = 'sticky';
-        projectImage.style.top = '20px';
-        projectImage.style.bottom = '20px'
-    } else {
-        projectImage.style.position = 'relative';
-        projectImage.style.top = '-100px'
-    }
-    if (rect2.top <= 140) {
-        projectImage.classList.add('fade-out');
-
-        setTimeout(() => {
-            projectImage.src = '../assets/media/Project img 1.png';
-            projectImage.classList.add('fade-in');
-        }, 400);
-    }
-    else{
-        projectImage.src = '../assets/media/project2.jpg';
-        
-    }
-    
-}
-
-window.addEventListener('scroll', onscroll);
 
 
